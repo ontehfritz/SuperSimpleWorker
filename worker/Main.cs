9abf436aa92addce35a8349a6a3a836460bba1b0
fritzcoder <fredrick.seitz@gmail.com>
2013-07-09 12:57:02 +0200

    it can now be run as a console app and installed as a service on windows close #4

--- worker/Main.cs
+++ worker/Main.cs
@@ -4,15 +4,59 @@ using DelayedJob;
 using System.Reflection;
 using System.Linq;
 using System.ServiceProcess;
+using System.Configuration.Install;
 using System.Diagnostics;
+using System.ComponentModel;
+using System.Threading;
 
 namespace DelayJob.Worker
 {
+	[RunInstaller(true)]
+	public class WorkerServiceInstaller : Installer
+	{
+		public WorkerServiceInstaller()
+		{
+			ServiceProcessInstaller process = new ServiceProcessInstaller();
+
+			process.Account = ServiceAccount.LocalSystem;
+
+			ServiceInstaller serviceAdmin = new ServiceInstaller();
+
+			serviceAdmin.StartType = ServiceStartMode.Manual;
+
+			serviceAdmin.ServiceName = "DelayedJobWorker";
+			serviceAdmin.DisplayName = "Delayed Job Worker";
+			serviceAdmin.Description = "Delayed job worker service. Runs jobs for delayed job.net";
+
+			Installers.Add(process);
+			Installers.Add(serviceAdmin);
+		}
+	}
 	/// <summary>
 	/// Main class.
 	/// </summary>
-	class Program
+	public class Program
 	{
+		private static Timer timer;
+
+		public class WorkerService : ServiceBase
+		{
+			public WorkerService()
+			{
+				ServiceName = "DelayedJobWorker";
+			}
+
+			protected override void OnStart(string[] args)
+			{
+				Program.Start(args);
+			}
+
+			protected override void OnStop()
+			{
+				Program.Stop();
+			}
+		}
+
 		private static bool work = true;
 		/// <summary>
 		/// The entry point of the program, where the program control starts and ends.
@@ -22,16 +66,35 @@ namespace DelayJob.Worker
 		/// </param>
 		public static void Main (string[] args){
 
-			try{
-				Start (args);
-			}catch(Exception e) {
-				throw e;
-			}
-			finally{
-				Stop ();
+			int platformID = (int) Environment.OSVersion.Platform;
+			//platformIDs 4, 6, 128 *nix based systems
+			//Environment.UserInteractive always false when running under mono
+			if (Environment.UserInteractive ||
+			    ((platformID == 4) || 
+			 	 (platformID == 6) || 
+			 	 (platformID == 128))){
+
+				try {
+					Start (args);
+					while(work)
+					{ 
+					
+					}
+				} catch (Exception e) {
+					throw e;
+				} finally {
+					Stop ();
+				}
+
+				Console.WriteLine ("Exiting ...");
+
+			}else {
+				//Console.WriteLine("Service");
+				// running as service
+				using (var service = new WorkerService())
+					ServiceBase.Run (service);
 			}
 
-			Console.WriteLine ("Exiting ...");
 		}
 
 		private static void Start(string[] args)
@@ -43,14 +106,12 @@ namespace DelayJob.Worker
 				Job.WorkerName = args [0];
 			}
 
-			int SLEEP = 5;
-
 			Assembly assembly = Assembly.Load("DelayedJob");
 			Type type = assembly.GetType ("DelayedJob." + providerName);
 
 			Job.Repository = (IRepository)Activator.CreateInstance(type,connectionString);
 
-			Console.WriteLine (connectionString);
+			//Console.WriteLine (connectionString);
 			Console.WriteLine (providerName);
 
 			Console.CancelKeyPress += delegate(object sender, ConsoleCancelEventArgs e) {
@@ -59,32 +120,39 @@ namespace DelayJob.Worker
 			};
 
 			Console.WriteLine ("*** Starting job worker " + Job.WorkerName);
+			timer = new Timer (new TimerCallback (Work),null, 5000, 5000);
+
+			//Console.ReadLine();
+
+		}
+
+		private static void Work(object state)
+		{
+			int SLEEP = 5;
 			Stopwatch benchmark = new Stopwatch ();
 
-			while(work)
-			{
-				benchmark.Start ();
-				Job.Report report = Job.WorkOff();
-				benchmark.Stop ();
-				TimeSpan ts = benchmark.Elapsed;
+			benchmark.Start ();
+			Job.Report report = Job.WorkOff();
+			benchmark.Stop ();
+			TimeSpan ts = benchmark.Elapsed;
 
-				if (report.failure == 0 &&
-				    report.success == 0) {
+			if (report.failure == 0 &&
+				report.success == 0) {
 					//sleep if no jobs 
-					System.Threading.Thread.Sleep (SLEEP * 1000); 
-				} else {
-					//if jobs have run then print out a report on success and failures
-					int count = report.success + report.failure;
-					double jobsPerSecond = (((double)ts.Milliseconds) / (count * 1.0)) / 1000;
-					Console.WriteLine (count.ToString() + " jobs processed at " + jobsPerSecond.ToString() +  
+				System.Threading.Thread.Sleep (SLEEP * 1000); 
+			} else {
+				//if jobs have run then print out a report on success and failures
+				int count = report.success + report.failure;
+				double jobsPerSecond = (((double)ts.Milliseconds) / (count * 1.0)) / 1000;
+				Console.WriteLine (count.ToString() + " jobs processed at " + jobsPerSecond.ToString() +  
 					                   " j/s, " + report.failure.ToString() + 
 					                   " failed ...");
-				}
 			}
 		}
 
 		private static void Stop()
 		{
+			timer.Dispose();
 			Job.ClearLocks();
 		}
 	}
